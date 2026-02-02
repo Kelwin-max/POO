66c619e67a4f59b995e1d43e0d3cf9669dd01ec0
Kelwin Gil Fernandes <kelwingilfernandes18@gmail.com>
2026-02-02 10:18:11 -0300

    Added files via commit

---
+++ E-commerce(3)/E-commerce/src/main/java/com/_DM/E_commerce/Entity/Pedido.java
@@ -0,0 +1,30 @@
+package com._DM.E_commerce.Entity;
+
+import com._DM.E_commerce.Enum.StatusDoPedido;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private LocalDate momento;
+    private StatusDoPedido status;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Usuario cliente;
+
+}
