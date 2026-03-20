b7bd8110bfbb7d4ca7d5d1f88e034d1243168a5b
Kelwin-max <kelwingilfernandes18@gmail.com>
2026-03-20 09:06:10 -0300

    Atualizei o codigo e completei

--- E-commerce(3)/E-commerce/src/main/java/com/_DM/E_commerce/Entity/Pedido.java
+++ E-commerce(3)/E-commerce/src/main/java/com/_DM/E_commerce/Entity/Pedido.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -27,4 +29,17 @@ public class Pedido {
     @JoinColumn(name = "cliente_id")
     private Usuario cliente;
 
+    @OneToOne(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private Pagamento pagamento;
+
+    @OneToMany(mappedBy = "id.pedido")
+    private Set<ItemDoPedido> itens = new HashSet<>();
+
+    public Double getTotal() {
+        double sum = 0.0;
+        for (ItemDoPedido x : itens) {
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
 }
